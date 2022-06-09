3e40a277bd587169818f4f985d19f807a9aa11e2
Anica Rahman <anicarahman2009@yahoo.it>
2022-06-09 15:36:42 +0100

    New movie created

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -10,6 +10,17 @@ const getMovies = async (req, res) => {
   res.json({ data: movies });
 };
 
+const createMovie = async (req, res) => {
+  const movie = await prisma.movie.create({
+    data: {
+      title: req.body.title,
+      runtimeMins: Number(req.body.runtimeMins),
+    },
+  });
+  res.json({ data: movie });
+};
+
 module.exports = {
   getMovies,
+  createMovie,
 };
